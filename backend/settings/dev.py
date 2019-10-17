b5873a487d8288928152390a5117cd9ea70b9246
metleeha <nodongth@gmail.com>
2019-10-17 16:41:04 +0900

    [ADD] pip auth 관련 modules 설치
    settings/dev.py 에 설정 저장
    S1P1151020-2
    Actions (Type . to access issue actions)

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -38,13 +38,30 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',  # < Per Whitenoise, to disable built in
     'django.contrib.staticfiles',
+    
+    # 로그인 기능을 위한 추가앱 
+    'django.contrib.sites',
+    'rest_auth',
+    'rest_auth.registration',
+
+    # SNS 로그인
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    # RESTful API 
     'rest_framework',
+    'rest_framework.authtoken',
+    'corsheaders',
+
+    # 생성한 앱들 
     'backend.api',
     'backend.posts',
     'backend.links',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # 반드시 최상단
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,6 +71,66 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# cors header setting 
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:8080"
+]
+
+COR_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT'
+)
+
+CORS_ALLOW_HEADERS = (
+'accept',
+'accept-encoding',
+'authorization',
+'access-control-request-method',
+'access-control-request-headers',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+)
+
+# auth model setting
+
+# AUTH_USER_MODEL = "app이름.앱에 있는 유저 모델 class 이름"
+# SITE_ID = 1
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework.authentication.TokenAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
+    'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+    'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+#JWT_AUTH 설정을 위해 settings.py 맨 위해 import datetime을 추가하자!!
+JWT_AUTH = {
+    # If the secret is wrong, it will raise a jwt.DecodeError telling you as such. You can still get at the payload by setting the JWT_VERIFY to False.
+    'JWT_VERIFY': True,
+    # You can turn off expiration time verification by setting JWT_VERIFY_EXPIRATION to False.
+    # If set to False, JWTs will last forever meaning a leaked token could be used by an attacker indefinitely.
+    'JWT_VERIFY_EXPIRATION': True,
+    # This is an instance of Python's datetime.timedelta. This will be added to datetime.utcnow() to set the expiration time.
+    # Default is datetime.timedelta(seconds=300)(5 minutes).
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+}
+REST_USE_JWT = True
+
 
 ROOT_URLCONF = 'backend.urls'
 
