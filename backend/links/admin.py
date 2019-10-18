12674daa8d89f929d5fc7ffb605a3db1e52e0413
metleeha <nodongth@gmail.com>
2019-10-18 10:23:07 +0900

    [ADD] links model links 모델과 label, tag 모델 구현 관리자 페이지에서 더미 데이터 입력 S1P1151020-55 Actions (Type . to access issue actions) S1P1151020-56 Actions (Type . to access issue actions)

--- backend/links/admin.py
+++ backend/links/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Link
+from .models import Label
+from .models import Tag
+from .models import LinkLabelJoin
+from .models import LinkTagJoin
+
 
 # Register your models here.
+admin.site.register(Link)
+admin.site.register(Tag)
+admin.site.register(Label)
+admin.site.register(LinkLabelJoin)
+admin.site.register(LinkTagJoin)
--- backend/links/models.py
+++ backend/links/models.py
@@ -1,2 +1,47 @@
 from django.db import models
-# from django.contrib.auth.models import User 
+from django.conf import settings
+
+class Link(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) 
+    url = models.TextField()
+    title = models.CharField(max_length=45, blank=True)
+    thumbnail = models.TextField(blank=True)
+    summary = models.TextField()
+    sharable = models.IntegerField(null=True) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return '[{}] {}'.format(self.user.nickname, self.title)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=45)
+    links = models.ManyToManyField('Link', through='LinkTagJoin', related_name='tags')
+
+    def __str__(self):
+        return self.name
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=45)
+    links = models.ManyToManyField('Link', through='LinkLabelJoin', related_name='labels')
+
+    def __str__(self):
+        return self.name
+
+
+class LinkTagJoin(models.Model):
+    link = models.ForeignKey(Link, related_name='link_from_tag', on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, related_name='tag_from_link', on_delete=models.CASCADE)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} & {} | {}".format(self.link.title, self.tag.name, self.date_joined)
+
+class LinkLabelJoin(models.Model):
+    link = models.ForeignKey(Link, related_name='link_from_label', on_delete=models.CASCADE)
+    label = models.ForeignKey(Label, related_name='label_from_link', on_delete=models.CASCADE)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return "{} & {} | {}".format(self.link.title, self.label.name, self.date_joined)
