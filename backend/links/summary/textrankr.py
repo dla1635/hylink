fc5f7114d5cb71f9e575d74eb303a986a5814fc9
심창훈 <dnasim34@naver.com>
2019-10-23 17:53:30 +0900

    [MOD] extract nouns에서 noun을 하나도 못뽑는 예외에 대한 처리 추가 Jira S1P1151020-71

--- backend/links/summary/textrankr.py
+++ backend/links/summary/textrankr.py
@@ -21,47 +21,69 @@ class TextRank(object):
 
     def build(self):
         self._build_sentences()
-        self._extract_nouns()
+        
+        self.has_nouns = self._extract_nouns()
+
+        # 문장 처리
         self._build_graph()
-        # 문장 랭크 처리
         self.pageranks = pagerank(self.graph, weight='weight')
         self.reordered = sorted(self.pageranks, key=self.pageranks.get, reverse=True)
-        # 단어 랭크 처리
-        self.word_rank_idx = self.get_word_ranks(self.words_graph)
-        self.sorted_word_rank_idx = sorted(self.word_rank_idx, key=lambda k: self.word_rank_idx[k], reverse=True)
+        
+        # 단어 처리
+        if self.has_nouns:
+            self._build_word_graph()
+            self.word_rank_idx = self.get_word_ranks(self.words_graph)
+            self.sorted_word_rank_idx = sorted(self.word_rank_idx, key=lambda k: self.word_rank_idx[k], reverse=True)
 
     def _build_sentences(self):
         dup = {}
-        # 0.1같은 실수형식이랑 .txt같은 파일형식 안잘리게 정규식으로 split
-        # 풀어쓰면 아래 주석과 같은 기능
-        # candidates = split(r'(?<=[^0-9])(?<=[^a-z])[\.|\n]', self.text)
+        #candidates = split(r'(?:(?<=[^0-9])\.|\n)', self.text)
+        #전체 text를 문장단위로 split한다
+        #   전체 문장 text를 \n으로 split
+        #   나눈 한 line에서 .으로 split
+        #   line에서 앞 뒤 공백 제거후 append
+        
         candidates = []
+        
         for enter_line in split('\n', self.text): 
             for line in split(r'(?<=[^0-9])(?<=[^a-z])[\.]', enter_line):
                 candidates.append(line.strip(' ').strip('.').strip('\t'))
 
         self.sentences = []
+        
         index = 0
+
         for candidate in candidates:
             if len(candidate) >= 1 and candidate not in dup:
                 dup[candidate] = True
                 self.sentences.append(Sentence(candidate + '.', index))
                 index += 1
-            
+                
         del dup
         del candidates
-    
+
     def _extract_nouns(self):
         okt = Okt()
         self.nouns = []
-        self.stopwords = ["아", "휴", "아이구", "아이쿠", "아이고", "어", "나", "우리", "저희", "따라", "의해", "을", "를", "에", "의", "가",]
+        self.stopwords = ["대한","아", "휴", "아이구", "아이쿠", "아이고", "어", "나", "우리", "저희", "따라", "의해", "을", "를", "에", "의", "가",]
         
         for sentence in self.sentences:
             if sentence.text is not '':
                 self.nouns.append(' '.join([noun for noun in okt.nouns(str(sentence.text))
                 if noun not in self.stopwords and len(noun) > 1]))
+        
+        # noun이 하나도 안뽑혔는지 재확인
+        # noun이 하나라도 있으면 true리턴
+        # 하나도 없으면 false리턴
+        for noun in self.nouns:
+            if noun:
+                return True
+        
+        return False
+
 
     def _build_graph(self):
+        
         #문장 그래프 처리
         self.graph = Graph()
         self.graph.add_nodes_from(self.sentences)
@@ -71,13 +93,16 @@ class TextRank(object):
             if weight:
                 self.graph.add_edge(sent1, sent2, weight=weight)
         
+    def _build_word_graph(self):
         #단어 그래프 처리
+        
         self.tfidf = TfidfVectorizer()
         self.cnt_vec = CountVectorizer()
         cnt_vec_mat = normalize(self.cnt_vec.fit_transform(self.nouns).toarray().astype(float), axis=0)
         vocab = self.cnt_vec.vocabulary_
         self.words_graph = np.dot(cnt_vec_mat.T, cnt_vec_mat)
         self.idx2word = {vocab[word] : word for word in vocab}
+
 
     def _jaccard(self, sent1, sent2):
         p = sum((sent1.bow & sent2.bow).values())
@@ -98,6 +123,7 @@ class TextRank(object):
         ranks = np.linalg.solve(A, B) # 연립방정식 Ax = b
         return {idx: r[0] for idx, r in enumerate(ranks)}
 
+
     def summarize(self, count=3, verbose=True):
         results = sorted(self.reordered[:count], key=lambda sentence: sentence.index)
         results = [result.text for result in results]
@@ -109,7 +135,10 @@ class TextRank(object):
     def keywords(self, word_num=3):
         
         keywords = []
-        for idx in self.sorted_word_rank_idx[:word_num]:
-            keywords.append(self.idx2word[idx])           
+        if self.has_nouns:
+            for idx in self.sorted_word_rank_idx[:word_num]:
+                keywords.append(self.idx2word[idx])           
         
         return keywords
+
+            
