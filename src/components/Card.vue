2e1250978b4216924b0b6fb35eeb5240b76b6ac0
limjongwan <dla1635@gmail.com>
2019-10-24 15:00:41 +0900

    [MOD] 카드 UI변경(hover시 링크이동,복사) Jira S1P1151020-81

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,40 +1,58 @@
-
 <template>
 <v-hover v-slot:default="{ hover }">
-  <v-card class="card" :elevation="hover ? 12 : 2" :color="hover ? '#F2F2F2': '#fff'">
-    <v-layout class="card_sub1">
-      <v-flex class="card_sub2">
-        <v-layout>
-          <v-flex  xs9 sm9 lg9 xl9>
-            <input v-if="editFlag" type="text" v-model="card.title">
-            <span v-if="!editFlag" style="font-size:20px;" @click="goLink">{{card.title}}</span><br/>
+  <v-card class="card" :elevation="hover ? 12 : 2">
+    <CardModifyModal v-if="modal_flag" :card="card"></CardModifyModal>
+    <div
+      v-if="hover"
+      class="d-flex v-card--reveal display-3"
+      style="height: 100%;  background-color:#0002;"
+    >
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+           <v-btn icon @click="goLink" v-on="on"><v-icon size=20>reply</v-icon></v-btn>
+        </template>
+        <span>링크 이동</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+           <v-btn icon @click="changeEditMode" v-on="on"><v-icon size=20>edit</v-icon></v-btn>
+        </template>
+        <span>편집</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+           <v-btn icon @click="copyToClipboard(card.url)" v-on="on"><v-icon size=20>file_copy</v-icon></v-btn>
+        </template>
+        <span>클립보드 복사</span>
+      </v-tooltip>
+    </div>
+      <v-layout class="card_sub1">
+        <v-flex class="card_sub2">
+          <v-layout>
+            <p id="card_title" v-if="!editFlag" style="font-size:20px;">{{card.title}}</p><br/>
+          </v-layout>
+          <v-flex v-show="summary_flag">
+            <p v-if="!editFlag" class="summary">{{card.summary}}</p>
           </v-flex>
-            <div style="display:inline-block; height:0px;" v-show="hover"  xs3 sm3 lg3 xl3>
-              <v-btn icon @click="changeEditMode"><v-icon size=20>edit</v-icon></v-btn>
-              <v-btn icon @click="copyToClipboard(card.url)"><v-icon size=20>file_copy</v-icon></v-btn>
-            </div>
-        </v-layout>
-        <v-flex v-show="summary_flag">
-          <v-textarea v-if="editFlag" dense rows="3" cols="38" v-model="card.summary"></v-textarea>
-          <!-- <input v-if="editFlag" type="text" v-model="card.summary"> -->
-          <p v-if="!editFlag" class="summary">{{card.summary}}</p>
         </v-flex>
+        <img v-show="thumnail_flag" class="card_img" :src="card.thumbnail" />
+      </v-layout>
+      <v-flex id="tagContainer">
+        <span class="tag"  style="font-size:70%; height:26px; margin:7px; padding:0 0px;" v-for="tag in card.tagList" :key="tag">#{{tag}}</span>
       </v-flex>
-      <img v-show="thumnail_flag" class="card_img" :src="card.thumbnail" />
-    </v-layout>
-        <v-flex id="tagContainer">
-          <span class="tag"  style="font-size:70%; height:26px; margin:7px; padding:0 0px;" v-for="tag in card.tagList" :key="tag">#{{tag}}</span>
-        </v-flex>
   </v-card>
   </v-hover>
 </template>
 
 <script>
+import CardModifyModal from '@/components/CardModifyModal'
+
 export default {
   name: "Card",
   data() {
     return{
       editFlag: false,
+      modal_flag: false,
     }
   },
   props: {
@@ -43,12 +61,15 @@ export default {
     thumnail_flag: { type: Boolean},
     summary_flag: { type: Boolean},
   },
+  components: {
+    CardModifyModal
+  },
   methods: {
     goLink() {
       location= this.card.url;
     },
     changeEditMode() {
-      this.editFlag?this.editFlag=false:this.editFlag=true;
+      this.modal_flag?this.modal_flag=false:this.modal_flag=true;
     },
     copyToClipboard(val) {
       var t = document.createElement("textarea");
@@ -66,16 +87,37 @@ export default {
 </script>
 
 <style scoped>
-.card {
-  margin: 10px 10px;
-  column-width: 300px;
-  height:143px;
+
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  z-index: 10;
 }
 
+.card {
+  margin: 10px 10px;
+}
 .card_sub1 {
 }
 .card_sub2 {
   padding:5px 0 0 5px;
+}
+
+#card_content {
+  position: relative;
+  top:0px;
+}
+
+#card_title {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 150px;
+  height: 30px;
+  margin:0px;
 }
 .card_img {
   width: 100px;
