d3978aa2c3fea9675fd915a8d628417f61e33407
심창훈 <dnasim34@naver.com>
2019-10-24 09:55:20 +0900

    [ADD] summary기능 test code 변경

--- backend/links/summary/test.py
+++ backend/links/summary/test.py
@@ -1,5 +1,6 @@
 import sys
 import io
+
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
@@ -9,39 +10,53 @@ from urllib import parse #인코딩용 임포트
 
 from bs4 import BeautifulSoup
 from textrankr import TextRank
-
 from url2text import urlparse
 
+max_keyword = 3
+
+url ="https://godoftyping.wordpress.com/2017/06/24/python-beautifulsoup/"
+# url = "https://ndb796.tistory.com/250"
+img_url, title, input_text, meta_tag = urlparse(url)
 
 
-url ="http://cloudpanya.com/221665855362"
-img_url, title, input_text = urlparse(url)
-# print("origin html text")
-# print(soup)
-# print("======= all tag ==========")
-# all_div = soup.find_all("div")
-
-# input_text = ""
-# for line in all_div:
-#     print(line.get_text().strip())
-#     input_text+= line.get_text().strip()
 
 
-# print("======= all p ==========")
-# for line in all_p:
-#     print(line.get_text().strip() +"\n")
 
+print("=============== title      ============")
+print("type : {}\nvalue : {}".format(type(title), title))
+print("=============== img-url    ============")
+print("type : {}\nvalue : {}".format(type(img_url), img_url))
+print("=============== start-ranking =========")
 
+if input_text=="None":
+    #택스트 없으면 종료
+    print("추출된 텍스트 없음!")
+    print(title)
+    print(img_url)
+    idx = 0
+    for keyword in meta_tag[:max_keyword]:
+        print("{}".format(meta_tag[idx]))
+        idx+=1
+    sys.exit(1)
+
+print("input_text is >>")
+print("type : {}".format(type(input_text)))
+print("value : {}".format(input_text[:100]))
 textrank = TextRank(input_text)
-# print(textrank.summarize())  # gives you some text
-# print(textrank.summarize(3, verbose=False))  # up to 3 sentences, returned as list
-print("===============    title   ============")
-print(title)
+print("=============== after-ranking =========")
 print("=============== summarized ============")
 sentences = textrank.summarize(3, verbose=False)
-print("1 : {}".format(sentences[0]))
-print("2 : {}".format(sentences[1]))
-print("3 : {}".format(sentences[2]))
+idx = 1
+print("type : {}".format(type(sentences)))
+for sentence in sentences:
+    print("{} : {}".format(idx,sentences[idx-1]))
+    idx+=1
+
 print("=============== keywords ==============")
-keywords = textrank.keywords(3)
-print("{} , {} , {}".format(keywords[0],keywords[1],keywords[2]))
+keywords = textrank.keywords(max_keyword)
+idx = 1
+print("type : {}".format(type(keywords)))
+for keyword in keywords:
+    print("{}".format(keywords[idx-1]))
+    idx+=1
+# print("{} , {} , {}".format(keywords[0],keywords[1],keywords[2]))
