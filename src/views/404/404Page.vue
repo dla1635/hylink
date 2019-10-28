fa7860acba38e25831bb08cea60f8583acbc524e
Jinhwan Seo <dicmon@naver.com>
2019-10-28 17:55:00 +0900

    [ADD] front_404 error page

---
+++ src/views/404/404Page.vue
@@ -0,0 +1,22 @@
+<template>
+<v-app id="container">
+    404 ERROR!
+</v-app>
+</template>
+
+<script>
+export default {
+    name: "404",
+    data() {
+        return {
+            
+        };
+    },
+};
+</script>
+
+<style scoped>
+#container {
+    background-color: white;
+}
+</style>
