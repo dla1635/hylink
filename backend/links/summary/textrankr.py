302c939a792415c0895b8c8f274bfdfb2235dee3
심창훈 <dnasim34@naver.com>
2019-10-29 17:27:32 +0900

    [ADD] 리턴할때 없는경우 "None"로 돌려주도록 함

--- backend/links/summary/textrankr.py
+++ backend/links/summary/textrankr.py
@@ -179,7 +179,11 @@ class TextRank(object):
     def summarize(self, count=3, verbose=True):
         results = sorted(self.reordered[:count], key=lambda sentence: sentence.index)
         results = [result.text for result in results]
-    
+        if len(results) < count:
+            for i in range(len(results),count):
+                results.append("None")
+                i += 1
+        
         if verbose:
             return '\n'.join(results)
         else:
@@ -187,24 +191,20 @@ class TextRank(object):
 
     def keywords(self, word_num=3):
         keywords = []
-        # if self.has_nouns == False:
-        #     return "None"
-
-        # for idx in self.sorted_word_rank_idx[:word_num]:
-        #         keywords.append(self.idx2word[idx])
+        
         keywords = self.word_rank_collections.most_common(word_num)
-       
-        return keywords
+        results = []
+        for keyword in keywords:
+            results.append(keyword[0])
+        if len(results) < word_num:    
+            for i in range(len(results),word_num):
+                    results.append("None")
+                    i += 1
+
+        return results
 
 if __name__ == "__main__":
-    input_text = '''외계 행성(外界行星) 또는 계외 행성(系外行星)은 태양계 밖의 행성으로, 태양이 아닌 다른 항성 주위를 공전하고 있는 행성이다. 지금까지 3800여 개의 외계 행성이 발견되었으며(2018년 6월 23일 기준: 행성계 2840개에서 행성 3796개. 이 중 다중행성계는 632개) 모두 우리 은하 내에 있다.[1] 우리 은하에만 수십억 개의 행성이 존재하는 것으로 추측되며[2][3][4] 대부분 항성을 돌고 있으나 일부는 홀로 우주 공간을 움직이는 떠돌이 행성이기도 하다.[5] 발견된 외계 행성들 중 지구와 가장 비슷한 것은 프록시마 b이다.
-
-수 세기에 걸쳐 많은 철학자와 과학자들은 외계 행성이 있으리라고 추측해 왔으나 이들이 얼마나 흔하게 있는지 또는 우리 태양계와 외계 행성계가 얼마나 비슷한지 알 방법이 없었다. 19세기부터 외계 행성을 찾았다는 발표가 여러 번 있었으나 천문학자들의 검증 결과 이 모든 주장들은 기각되었다. 1992년 펄서 PSR B1257+12 주위를 도는 암석 행성들의 존재가 최초로 검증, 발표되었다. 주계열성을 도는 행성 중 최초로 확인된 행성은 페가수스자리 51을 4일에 한 바퀴 도는 가스 행성 페가수스자리 51 b이다. 관측 기술의 향상 덕분에 이후 외계 행성의 발견 속도는 상승했다. 몇몇 외계 행성은 망원경으로 직접 사진을 찍었으나 대다수는 시선 속도와 같은 간접적인 방법으로 발견되었다.[1]
-
-확인된 외계 행성 대부분은 목성 또는 해왕성 정도 덩치의 가스 행성으로 추측되나 가스 행성이 외계 행성들 중 대부분을 차지한다는 의미는 아니다. 단지 무거운 행성들은 쉽게 눈에 띄기 때문이며 선택 편향의 결과이다.[6] 상대적으로 가벼운 지구질량 수 배 정도의 외계 행성들도 많이 발견되었으며 통계적 연구결과 이들 암석형 외계 행성의 수는 가스 행성보다 많은 것으로 보인다.[7] 최근 지구와 비슷하거나 작은 질량의 행성들도 발견되고 있으며 이들 중 일부는 질량 외의 여러 속성이 지구와 비슷한 것도 있다.[8][9][10] 갈색 왜성을 도는 외계 행성들도 있으며 어떤 항성에도 속박되지 않고 우주를 떠도는 행성도 있다. 그러나 이런 특수한 상황에서 천체들에 '행성' 명칭이 항상 적용되는 것은 아니다.
-
-일부 행성은 생명체 거주가능 영역 내를 돌고 있어 표면에 액체 물(또는 생명체)이 존재 가능할 것으로 보이며, 이런 행성들의 발견으로 외계 생명체의 존재 여부에 대한 관심은 증폭되고 있다.[11] 외계 행성이 생명체를 품기에 적합한지의 폭넓은 요소들을 고려하는 것을 행성 거주 가능성 연구라고 하며 이는 외계 행성 탐사에 포함된다.
-'''
+    input_text = ''''''
     textrank = TextRank(input_text)
 
     print("=============== summarized ============")
