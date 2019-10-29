039510ef8294ae651dc064a6e1a55b4e27680384
Jinhwan Seo <dicmon@naver.com>
2019-10-29 10:57:46 +0900

    [ADD] NaviGuard & NotFoundPage

--- src/config/router.js
+++ src/config/router.js
@@ -7,9 +7,11 @@ import Welcome from '@/views/Welcome/WelcomePage'
 import Login from '@/views/Login/LoginPage'
 import Regist from '@/views/Regist/RegistPage'
 
+import NotFound from '@/views/Error/NotFound404'
+
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode : 'history',
   routes: [
     {
@@ -20,17 +22,43 @@ export default new Router({
     {
       path: '/home',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {authRequired: true}
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta: {authRequired: true}
     },
     {
       path: '/regist',
       name: 'regist',
-      component: Regist
-    }
+      component: Regist,
+    },
+    {
+      path: '/404',
+      name: 'notfound',
+      component : NotFound
+    },
+    {
+      path: '*',
+      redirect: '/404' 
+    },
   ]
 })
+
+router.beforeEach(function (to, from, next) {
+  // to: 이동할 url에 해당하는 라우팅 객체
+  if (to.matched.some(function(routeInfo) {
+    return routeInfo.meta.authRequired;
+  })) {
+    // 이동할 페이지에 인증 정보가 필요하면 경고 창을 띄우고 페이지 전환은 하지 않음
+    alert('Login Please!');
+    next(false);
+  } else {
+    next(); // 페이지 전환
+  }
+});
+
+export default router
