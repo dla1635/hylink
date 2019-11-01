e84d5790b2e15b568318dfaaad80c92d3a6266c6
donghyun <dh442184@gmail.com>
2019-11-01 15:36:20 +0900

    [MOD] ShareViewSet 수정중

--- backend/sharing/views.py
+++ backend/sharing/views.py
@@ -14,4 +14,16 @@ class ShareViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
 
     def create(self, request):
+        user = request.user
+        link_list = request.data.get('link_list', None)
+        ###### user & linklist 유효성 검사 ######
+
+        # 1. Share 객체 만들기
+        new_share = Share(user=user)
+        new_share.save()
+
+        for l_id in link_list:
+            link = Link.objects.get(id=l_id)
+            LinkGroup(group=new_share, link=link).save()
+
         return Response(status=status.HTTP_200_OK)
