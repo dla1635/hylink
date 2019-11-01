3f29cdf74759b0d93a705c6e636ed1b12c20494f
metleeha <nodongth@gmail.com>
2019-11-01 12:25:56 +0900

    [FIX] I want to die...

--- backend/profiles/serializers.py
+++ backend/profiles/serializers.py
@@ -2,129 +2,116 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
 
+from allauth.account import app_settings as allauth_settings
+from allauth.utils import email_address_exists
+from allauth.account.adapter import get_adapter
+from allauth.account.utils import setup_user_email
 
-# User Serializer
-# 유저(사용자) 시리얼라이저는 간단하기도하고 Delete serializer와도 매우 유사합니다.
-class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        max_length=128,
-        min_length=8,
-        write_only=True
-    )
+from rest_auth.registration.serializers import RegisterSerializer
+from rest_auth.serializers import LoginSerializer, UserDetailsSerializer
+from rest_auth.registration.views import RegisterView
+
+UserModel = get_user_model()
+
+
+class UserSerializer(UserDetailsSerializer):
+    """
+    User model w/o password
+    """
     class Meta:
-        model = get_user_model()
-        fields = ('email', 'username', 'password', 'token',)
-        read_only_fields = ('token',)
-
-    def update(self, instance, validated_data):
-        """Performs an update on a User."""
-
-        # Passwords should not be handled with `setattr`, unlike other fields.
-        # Django provides a function that handles hashing and
-        # salting passwords. That means
-        # we need to remove the password field from the
-        # `validated_data` dictionary before iterating over it.
-        password = validated_data.pop('password', None)
-
-        for (key, value) in validated_data.items():
-            # For the keys remaining in `validated_data`, we will set them on
-            # the current `User` instance one at a time.
-            setattr(instance, key, value)
-
-        if password is not None:
-            # `.set_password()`  handles all
-            # of the security stuff that we shouldn't be concerned with.
-            instance.set_password(password)
-
-        # After everything has been updated we must explicitly save
-        # the model. It's worth pointing out that `.set_password()` does not
-        # save the model.
-        instance.save()
-
-        return instance
+        model = UserModel
+        fields = ('pk', 'username', 'email')
+        read_only_fields = ('email', )
 
 
-# Register Serializer
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        max_length=128,
-        min_length=8,
-        write_only=True
-    )
-    token = serializers.CharField(max_length=255, read_only=True)
-    class Meta:
-        model = get_user_model()
-        fields = ('email', 'username', 'password', 'token')
+class RegisterSerializer(RegisterSerializer):
+    email = serializers.EmailField(required=allauth_settings.EMAIL_REQUIRED)
+    username = serializers.CharField(required=True, write_only=True)
+    password1 = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+
+    def validate_email(self, email):
+        email = get_adapter().clean_email(email)
+        if allauth_settings.UNIQUE_EMAIL:
+            if email and email_address_exists(email):
+                raise serializers.ValidationError(
+                    _("A user is already registered with this e-mail address."))
+        return email
+
+    def validate_password1(self, password):
+        return get_adapter().clean_password(password)
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError(
+                _("The two password fields didn't match."))
+        return data
+
+    def get_cleaned_data(self):
+        return {
+            'password1': self.validated_data.get('password1', ''),
+            'username': self.validated.data.get('username', ''),
+            'email': self.validated_data.get('email', ''),
+        }
+
+    def save(self, request):
+        adapter = get_adapter()
+        user = adapter.new_user(request)
+        self.cleaned_data = self.get_cleaned_data()
+        adapter.save_user(request, user, self)
+        setup_user_email(request, user, [])
+        user.profile.save()
+        return user
+
+
+# class RegisterSerializer(serializers.ModelSerializer):
+#     password = serializers.CharField(
+#         max_length=128,
+#         min_length=8,
+#         write_only=True
+#     )
+#     token = serializers.CharField(max_length=255, read_only=True)
+#     class Meta:
+#         model = get_user_model()
+#         fields = ('email', 'username', 'password', 'token')
  
-    def create(self, validated_data):
-        # Use the `create_user` method we wrote earlier to create a new user.
-        return get_user_model().objects.create_user(**validated_data)
+#     def create(self, validated_data):
+#         return get_user_model().objects.create_user(**validated_data)
 
 
-# Login Serializer
-# 여기선 ModelSerializer로 선언하지 않습니다.
-# 새롭게 만드는 것이 아닌 validate 즉, 로그인시 인증하는 용도이기 때문입니다.
-class LoginSerializer(serializers.Serializer):
+class LoginSerializer(LoginSerializer):
     email = serializers.CharField(max_length=255)
     username = serializers.CharField(max_length=255, read_only=True)
     password = serializers.CharField(max_length=128, write_only=True)
     token = serializers.CharField(max_length=255, read_only=True)
  
     def validate(self, data):
-        # The `validate` method is where we make sure that the current
-        # instance of `LoginSerializer` has "valid". In the case of logging a
-        # user in, this means validating that they've provided an email
-        # and password and that this combination matches one of the users in
-        # our database.
         email = data.get('email', None)
         password = data.get('password', None)
 
-        # Raise an exception if an
-        # email is not provided.
         if email is None:
             raise serializers.ValidationError(
                 'An email address is required to log in.'
             )
 
-        # Raise an exception if a
-        # password is not provided.
         if password is None:
             raise serializers.ValidationError(
                 'A password is required to log in.'
             )
-
-        # The `authenticate` method is provided by Django and handles checking
-        # for a user that matches this email/password combination. Notice how
-        # we pass `email` as the `username` value since in our User
-        # model we set `USERNAME_FIELD` as `email`.
         user = authenticate(username=email, password=password)
 
-        # If no user was found matching this email/password combination then
-        # `authenticate` will return `None`. Raise an exception in this case.
         if user is None:
             raise serializers.ValidationError(
                 'A user with this email and password was not found.'
             )
 
-        # Django provides a flag on our `User` model called `is_active`. The
-        # purpose of this flag is to tell us whether the user has been banned
-        # or deactivated. This will almost never be the case, but
-        # it is worth checking. Raise an exception in this case.
         if not user.is_active:
             raise serializers.ValidationError(
                 'This user has been deactivated.'
             )
 
-        # The `validate` method should return a dictionary of validated data.
-        # This is the data that is passed to the `create` and `update` methods
-        # that we will see later on.
         return {
             'email': user.email,
             'username': user.username,
             'token': user.token
         }
-        # def validate(self, data):
-        #     user = authenticate(**data)
-        #     if user and user.is_active:
-        #         return user
-        #     raise serializers.ValidationError("Incorrect Credentials")
