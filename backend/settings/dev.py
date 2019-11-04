f2b9a385f920593d16dca96eeb9637b1cf1227cf
심창훈 <dnasim34@naver.com>
2019-11-04 11:24:10 +0900

    [MOD] cors이슈해결중

--- backend/settings/dev.py
+++ backend/settings/dev.py
@@ -107,10 +107,11 @@ MIDDLEWARE = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-# CORS_ORIGIN_WHITELIST = (
-#     'localhost:8080',
-#     'localhost',
-# )
+CORS_ORIGIN_WHITELIST = [
+    'localhost:8080',
+    'localhost',
+    'chrome://',
+]
 
 CORS_URLS_REGEX = r'^/api/.*$'
 
