55081055d6c082cc937722008b57bc37c62f6c01
Jinhwan Seo <dicmon@naver.com>
2019-11-04 10:41:00 +0900

    [DEL] backend_delete sharing folder
    미완성인 sharing 기능이 추가되어 일단 삭제합니다.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
 <v-app>
 
-<<<<<<< HEAD
-=======
-    <!-- 상단 바 -->
-    <AppBar ></AppBar>
-
-    <!--  Side bar    -->
-    <NavBar  />
-
-    <!-- 웰컴 바 -->
-    
-
->>>>>>> a704d32957696c8cca6b8826c39f9eb4dc142659
     <v-content>
         <router-view />
     </v-content>
@@ -25,13 +13,7 @@
 export default {
     name: 'App',
     data: () => ({
-<<<<<<< HEAD
         
-=======
-        navBarDrawer: false,
-        AppBarDrawer: false,
-        isLogin: false,
->>>>>>> a704d32957696c8cca6b8826c39f9eb4dc142659
     }),
     components: {
         
