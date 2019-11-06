fe4222aa422cd2b8aae0d8de451b881355f0a1cf
limjongwan <dla1635@gmail.com>
2019-11-06 11:08:59 +0900

    [MOD] Conflict 문제 해결

--- src/views/Landing/AppBar.vue
+++ src/views/Landing/AppBar.vue
@@ -3,19 +3,20 @@
 
     <router-link to="home/label/total">
         <v-img src="@/assets/logo.png" max-width="50"></v-img>
-
     </router-link>
     <router-link to="home/label/total">
         <v-img src="@/assets/txtLogo.png" max-width="100"></v-img>
-        <v-btn text depressed to="login" color="#009CEE">
-            Login
-        </v-btn>
-        <v-btn v-if="islogin" text depressed to="logout" color="#009CEE">
-            Logout
-        </v-btn>
-        <v-btn text depressed to="regist" color="#009CEE">
-            Register
-        </v-btn>
+    </router-link>
+    <v-img src="@/assets/txtLogo.png" max-width="100"></v-img>
+    <v-btn text depressed to="login" color="#009CEE">
+        Login
+    </v-btn>
+    <v-btn v-if="islogin" text depressed to="logout" color="#009CEE">
+        Logout
+    </v-btn>
+    <v-btn text depressed to="regist" color="#009CEE">
+        Register
+    </v-btn>
 
     </v-app-bar>
 </template>
