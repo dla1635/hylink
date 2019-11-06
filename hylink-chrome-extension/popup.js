db273267ef771a1f5aa1eb2b0ea5825a3311d3b1
심창훈 <dnasim34@naver.com>
2019-11-06 14:43:19 +0900

    [MOD] 익스탠션 기능 수정

--- hylink-chrome-extension/popup.js
+++ hylink-chrome-extension/popup.js
@@ -25,54 +25,51 @@ chrome.tabs.getSelected(null, function(tab) {
 
 
 function loginServer(){
-  
-  // alert("a")
-  
+  // document.cookie = 'cross-site-cookie=bar; SameSite=None; Secure';
   console.log("login");
 
-  var postUrl = "http://localhost:8000/auth/login/";
-  // alert("b")
+  var postUrl = "http://13d31f75.ngrok.io/auth/login/";
   var xhr = new XMLHttpRequest();
   xhr.open('POST', postUrl, true);
-  
   var email = encodeURIComponent(document.getElementById("email").value);
   var password = encodeURIComponent(document.getElementById("password").value);;
   var params = 'email=' + email + '&password=' + password
   params = params.replace(/%20/g, '+');
 
-  // Set correct header for form data 
-  // xhr.setRequestHeader('Content-type', 'application/json');
   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  // alert("gogo");
-  console.log("login",params);
-  
+
   // Handle request state change events
   xhr.onreadystatechange = function() { 
     // If the request completed
     if (xhr.readyState == 4) {
-          console.log('ready');
+      console.log('ready');
       if (xhr.status == 200) {
-        // If it was a success, close the popup after a short delay
         // 로그인 완료 처리
-              // console.log("end");
-              // localStorage.setItem("TOKEN_STORAGE_KEY","savingval")
-              var json = JSON.parse(xhr.response);
-              // console.log(json.token);
-              
-              localStorage.setItem("TOKEN_STORAGE_KEY",JSON.stringify(json));
-              // localStorage.setItem("userId",json.user.email);
-              alert("로그인완료");
-              window.setTimeout(window.close, 1000);
-              
-          } else {
-              // Show what went wrong
-              // console.log('Error login: ',xhr.statusText);
-              // alert("error");
-              
-          }
-      }else{
-        // console.log(xhr.readyState);
-        // alert("what?")
+        // console.log("end");
+        // localStorage.setItem("TOKEN_STORAGE_KEY","savingval")
+        var json = JSON.parse(xhr.response);
+        // localStorage.setItem("TOKEN_STORAGE_KEY",JSON.stringify(json));
+        console.log(xhr.getAllResponseHeaders());
+        
+        if (xhr.getAllResponseHeaders().indexOf("csrftoken") >= 0) {
+          console.log("have");
+          console.log("header:", xhr.getResponseHeader("csrftoken"));
+          var header = xhr.getResponseHeader('csrftoken');
+        }
+
+        console.log("abcd");
+        console.log(header);
+        localStorage.setItem("userId",json.user.email);
+        localStorage.setItem("TOKEN_STORAGE_KEY",JSON.stringify(json));
+        alert("로그인완료");
+        document.getElementById('loginform').style.display='none';
+        document.getElementById('linkform').style.display='block';
+        // window.setTimeout(window.close, 1000);
+      } else {
+        // Show what went wrong
+        console.log('Error login: ',xhr.statusText);
+        // alert("error");      
+        }
       }
   };
 
@@ -83,23 +80,22 @@ function loginServer(){
 
 function addlink(){
   console.log("addlink");
-  var postUrl = "http://localhost:8000/api/link/";
-  // alert("b")
+  var postUrl = "http://13d31f75.ngrok.io/api/exlink/";
+  // var csrftoken = getCookie('csrftoken');
+  
+  // print("csrf:",csrftoken);
   var xhr = new XMLHttpRequest();
   xhr.open('POST', postUrl, true);
   var linkurl = encodeURIComponent(document.getElementById("url").value);
   console.log(linkurl);
   var json = JSON.parse(localStorage.getItem("TOKEN_STORAGE_KEY"))
-  var user ={
-    "email" : json.user.email, 
-    "pk" : json.user.pk,
-  }
+ 
   linkurl = linkurl.replace(/%20/g, '+');
-  var title ="";
-  var thumbnail ="";
-  var summary="";
-  var sharable=null;
-  var params = "url="+ linkurl+"&token="+json.token + "&title="+title+"&thumbnail="+thumbnail+"&summary="+summary+"&sharable="+sharable+"&user={email="+json.user.email+"}";
+  var title= "";
+  var thumbnail= "";
+  var summary= "";
+  var sharable= null;
+  var params= "email="+json.user.email+"&url=" + linkurl+"&title=" + title+"&thumbnail=" + thumbnail+"&summary=" + summary+"&sharable=" + sharable;  
   // var params = {
   //   user : {
   //     email : json.user.email, 
@@ -112,8 +108,8 @@ function addlink(){
   //   summary: "",
   //   sharable: null
   // } 
-  console.log(params);
-  console.log(JSON.stringify(params));
+  // console.log(params);
+  // console.log(JSON.stringify(params))
   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   // alert("gogo");
   // console.log("save",params);
@@ -127,7 +123,7 @@ function addlink(){
         
         console.log("end");
         alert("저장완료");
-        window.setTimeout(window.close, 1000);
+        // window.setTimeout(window.close, 1000);
               
       }else{
         console.log(xhr.statusText);
@@ -135,9 +131,88 @@ function addlink(){
     }
   };
   xhr.send(params);
+}
 
+function addlinkGet(){
+  console.log("addlink by get");
+  var getUrl = "http://13d31f75.ngrok.io/api/exlink/";
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', getUrl, false);
+  var linkurl = encodeURIComponent(document.getElementById("url").value);
+  console.log(linkurl);
+  var json = JSON.parse(localStorage.getItem("TOKEN_STORAGE_KEY"))
+  
+  var params= "email="+json.user.email+"&url=" + linkurl;  
+  params = params.replace(/%20/g, '+');
+  
+  console.log(params);
+  xhr.send(params);
 
 }
+
+// chrome.tabs.executeScript({
+//   code: 'performance.getEntriesByType("resource").map(e => e.name)',
+// }, data => {
+//   if (chrome.runtime.lastError || !data || !data[0]) return;
+  
+//   const urls = data[0].map(url => url.split(/[#?]/)[0]);
+ 
+//   const uniqueUrls = [...new Set(urls).values()].filter(Boolean);
+
+//   Promise.all(
+//     uniqueUrls.map(url =>
+//       new Promise(resolve => {
+//         chrome.cookies.getAll({url}, resolve);
+//       })
+//     )
+//   ).then(results => {
+//     // convert the array of arrays into a deduplicated flat array of cookies
+//     const cookies = [
+//       ...new Map(
+//         [].concat(...results)
+//           .map(c => [JSON.stringify(c), c])
+//       ).values()
+//     ];
+
+//     // do something with the cookies here
+//     console.log(uniqueUrls, cookies);
+//   });
+// });
+
+
+// function getCookie(name) {
+//   var cookieValue = null;
+//   if (document.cookie && document.cookie !== '') {
+//     console.log("A");
+//       var cookies = document.cookie.split(';');
+//       for (var i = 0; i < cookies.length; i++) {
+//           var cookie = cookies[i].trim();
+//           // Does this cookie string begin with the name we want?
+//           if (cookie.substring(0, name.length + 1) === (name + '=')) {
+//               cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//               break;
+//           }
+//       }
+//   }else{
+    
+
+//   }
+//   console.log("B");
+//   return cookieValue;
+// }
+
+// var setCookie = function(name, value, exp) {
+//   var date = new Date();
+//   date.setTime(date.getTime() + exp*24*60*60*1000);
+//   document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+//   };
+  
+// var getCookie = function(name) {
+//   var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+//   return value? value[2] : null;
+//   };
+  
 
 window.addEventListener('load',function(evt){
   document.getElementById('loginButton').addEventListener('click', loginServer);
